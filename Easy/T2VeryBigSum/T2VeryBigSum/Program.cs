6ae13e82dd811b0d157745112977f0e71b25f982
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-05-17 17:39:33 +0300

    created T2VeryBigSum

---
+++ Easy/T2VeryBigSum/T2VeryBigSum/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace T2VeryBigSum
+{
+    using System.IO;
+    using System;
+
+    public class Solution
+    {
+
+        // Complete the aVeryBigSum function below.
+        public static long AVeryBigSum()
+        {
+
+        }
+
+        private static void Main()
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            var arCount = Convert.ToInt32(Console.ReadLine());
+            var ar = Array.ConvertAll(Console.ReadLine().Split(' '), Convert.ToInt64);
+            var result = AVeryBigSum(ar);
+            textWriter.WriteLine(result);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
---
+++ Easy/T2VeryBigSum/T2VeryBigSum/Readme.txt
@@ -0,0 +1,17 @@
+﻿Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+Function Description
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+aVeryBigSum has the following parameter(s):
+ar: an array of integers .
+Input Format
+The first line of the input consists of an integer n. 
+The next line contains n space-separated integers contained in the array.
+Output Format
+Print the integer sum of the elements in the array.
+
+All the existing text of the program was together with the task*
+
+Solution:
+	- Create Unit Tests;
+	Program.cs:
+		- Implement an estimation method.
