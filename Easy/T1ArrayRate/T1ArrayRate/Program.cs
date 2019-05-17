a895ad042e6d1ea38fce1b24f8ff2c3c03943840
Anton Ahmetgaliev <antonahmetgaliev4@gmail.com>
2019-05-17 16:46:39 +0300

    created T1ArrayRate

---
+++ Easy/T1ArrayRate/T1ArrayRate/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace T1ArrayRate
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System;
+
+    internal class Solution
+    {
+        // Complete the compareTriplets function below.
+        static List<int> CompareTriplets(List<int> a, List<int> b)
+        {
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            var a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            var b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            var result = CompareTriplets(a, b);
+            textWriter.WriteLine(string.Join(" ", result));
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
---
+++ Easy/T1ArrayRate/T1ArrayRate/Readme.txt
@@ -0,0 +1,15 @@
+﻿Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a 
+scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+We define the rating for Alice's challenge to be the triplet , and the rating for Bob's challenge to be the triplet .
+Your task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+Comparison points is the total points a person earned.
+
+All the existing text of the program was together with the task*
+
+Solution:
+	- Create Unit Tests;
+	Program.cs:
+		- Implement an estimation method.
